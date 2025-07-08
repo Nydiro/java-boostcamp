e57e1d3a39752b8d6b401550b73cfda19ee5783a
Nydiro <tilmannvondrehle@gmail.com>
2025-07-08 20:17:12 +0200

    variables and scanner input

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Variablen
+        String nameString = "Nydiro";
+        int age = 28;
+
+        // Ausgabe
+        System.out.println("Hallo, mein Name ist " + nameString + " und ich bin " + age + " Jahre alt.");
+
+        // Scanner Objekt für User-Input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bitte geben Sie Ihren Namen ein: ");
+        String nameInputString = scanner.nextLine();
+
+        System.out.println("Bitte geben Sie Ihr Alter ein: ");
+        int ageInput = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println("Hallo, mein Name ist " + nameInputString + " und ich bin " + ageInput + " Jahre alt.");
     }
 }
